e50b9b35ebd9e94dc6873548e50121f9b8522e85
kthomas <kthomas1208@gmail.com>
2015-10-08 04:20:06 -0400

    Initial commit
    Has blank activity and a TextView in the fragment that says "Hello blank fragment"

---
+++ app/src/main/java/com/dreammist/popularmovies_stage1/FragmentMain.java
@@ -0,0 +1,26 @@
+package com.dreammist.popularmovies_stage1;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentMain extends Fragment {
+
+
+    public FragmentMain() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootView;
+    }
+
+}
