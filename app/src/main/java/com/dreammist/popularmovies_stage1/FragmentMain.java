251b6031d7ca43563134e6339a44fe061e642ac0
kthomas <kthomas1208@gmail.com>
2015-10-09 06:27:34 -0400

    Using ArrayAdapter and layout XML

--- app/src/main/java/com/dreammist/popularmovies_stage1/FragmentMain.java
+++ app/src/main/java/com/dreammist/popularmovies_stage1/FragmentMain.java
@@ -25,11 +25,11 @@ public class FragmentMain extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-//        ArrayAdapter mPosterAdapter = new ArrayAdapter<String>(
-//                getActivity(),                      //Context
-//                R.layout.grid_item_poster,          //ID of image layout
-//                R.id.grid_item_poster_imageview,    //ID of ImageView
-//                new ArrayList<String>());           //list of data (initially blank)
+        ArrayAdapter mPosterAdapter = new ArrayAdapter<String>(
+                getActivity(),                      //Context
+                R.layout.grid_item_poster,          //ID of image layout
+                R.id.grid_item_poster_imageview,    //ID of ImageView
+                new ArrayList<String>());           //list of data (initially blank)
 
         // Get the gridview and set the adapter to either ImageAdapter or ArrayAdapter (with image)
         GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
