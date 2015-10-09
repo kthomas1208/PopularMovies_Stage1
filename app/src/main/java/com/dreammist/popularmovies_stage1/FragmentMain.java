c9ee5463cac54ceeb5bdef3323edfd37b7ef9ef7
kthomas <kthomas1208@gmail.com>
2015-10-09 06:11:51 -0400

    Added layout xml for ImageView

--- app/src/main/java/com/dreammist/popularmovies_stage1/FragmentMain.java
+++ app/src/main/java/com/dreammist/popularmovies_stage1/FragmentMain.java
@@ -7,10 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class FragmentMain extends Fragment {
 
+    ArrayAdapter mPosterAdapter;
 
     public FragmentMain() {
         // Required empty public constructor
@@ -20,6 +24,14 @@ public class FragmentMain extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayAdapter mPosterAdapter = new ArrayAdapter<String>(
+                getActivity(),                      //Context
+                R.layout.grid_item_poster,          //ID of image layout
+                R.id.grid_item_poster_imageview,    //ID of ImageView
+                new ArrayList<String>());           //list of data (initially blank)
+
+        // Get the gridview and set the adapter to either ImageAdapter or ArrayAdapter (with image)
         GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
         gridView.setAdapter(new ImageAdapter(container.getContext()));
 
