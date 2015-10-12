9ba11973c7cbc035576345e31c13ca4848cd924c
kevinthomas <kthomas1208@gmail.com>
2015-10-12 02:18:03 -0400

    * Added FetchMoviesTask to use movie api * Built URI for fetching data * Added Picasso library * Loading images from Picasso with hardcoded image strings * API key removed from version control for legal purposes
    /* BUGS */
    * Images not being sized properly

--- app/src/main/java/com/dreammist/popularmovies_stage1/FragmentMain.java
+++ app/src/main/java/com/dreammist/popularmovies_stage1/FragmentMain.java
@@ -1,13 +1,20 @@
 package com.dreammist.popularmovies_stage1;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class FragmentMain extends Fragment {
@@ -23,8 +30,6 @@ public class FragmentMain extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        // TODO: 10/9/2015 Fill up an array of images
-
         mPosterAdapter = new ArrayAdapter<String>(
                 getActivity(),                      //Context
                 R.layout.grid_item_poster,          //ID of image layout
@@ -35,7 +40,68 @@ public class FragmentMain extends Fragment {
         GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
         gridView.setAdapter(new ImageAdapter(rootView.getContext()));
 
+        // TODO: 10/12/15 create method to parse JSON
+//        FetchMoviesTask fetchMoviesTask = new FetchMoviesTask();
+//        fetchMoviesTask.execute();
+
         return rootView;
     }
 
+    public class FetchMoviesTask extends AsyncTask<Void, Void, Void> {
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string
+            String moviesJsonStr = null;
+
+            final String SORT_PARAM = "sort_by";
+            final String API_PARAM = "api_key";
+
+            String sort = "popularity.desc";
+            String apiKey = "";
+
+            try {
+                // Build the URI for the API call
+                // http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3695e86fa2fd999053d25829965eccc4
+                Uri.Builder uriBuilder = new Uri.Builder();
+                uriBuilder.scheme("http");
+                uriBuilder.authority("api.themoviedb.org");
+                uriBuilder.appendPath("3");
+                uriBuilder.appendPath("discover");
+                uriBuilder.appendPath("movie");
+                uriBuilder.appendQueryParameter(SORT_PARAM, sort);
+                uriBuilder.appendQueryParameter(API_PARAM, apiKey);
+
+                URL url = new URL(uriBuilder.build().toString());
+
+                Log.v(LOG_TAG, url.toString());
+
+            }catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the data, there's no point in attempting
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+    }
 }
+
+
--- app/src/main/java/com/dreammist/popularmovies_stage1/ImageAdapter.java
+++ app/src/main/java/com/dreammist/popularmovies_stage1/ImageAdapter.java
@@ -8,7 +8,17 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-public class ImageAdapter extends BaseAdapter {
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImageAdapter<T> extends BaseAdapter {
+    /**
+     * Contains the list of objects that represent the data of this ArrayAdapter.
+     * The content of this list is referred to as "the array" in the documentation.
+     */
+    private List<T> mObjects;
+
     private Context mContext;
 
     public ImageAdapter(Context c) {
@@ -20,12 +30,21 @@ public class ImageAdapter extends BaseAdapter {
         mImages = i;
     }
 
+    /**
+     * Adds data to the list of objects
+     *
+     * @param object the list item to be added
+     */
+    public void add(T object) {
+        mObjects.add(object);
+    }
+
     public int getCount() {
-        return mImages.length;
+        return dummyStrings.length;
     }
 
     public Object getItem(int position) {
-        return null;
+        return mObjects.get(position);
     }
 
     public long getItemId(int position) {
@@ -50,7 +69,16 @@ public class ImageAdapter extends BaseAdapter {
             imageView = (ImageView) convertView;
         }
 
-        imageView.setImageResource(mImages[position]);
+        final String path = "http://image.tmdb.org/t/p/w185/";
+        //String url = path + getItem(position).toString();
+        String url = path + dummyStrings[position];
+
+        // TODO: 10/12/15 use grid_item_poster instead  
+
+        // Load the image into the ImageView using Picasso
+        Picasso.with(parent.getContext()).load(url).into(imageView);
+
+        //imageView.setImageResource(mImages[position]);
         return imageView;
     }
 
@@ -89,5 +117,20 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.interstellar, R.drawable.interstellar,
             R.drawable.interstellar, R.drawable.interstellar,
             R.drawable.interstellar, R.drawable.interstellar
+    };
+
+    private String[] dummyStrings = {
+            "jjBgi2r5cRt36xF6iNUEhzscEcb.jpg",
+            "AjbENYG3b8lhYSkdrWwlhVLRPKR.jpg",
+            "kqjL17yufvn9OVLyXYpvtyrFfak.jpg",
+            "qey0tdcOp9kCDdEZuJ87yE3crSe.jpg",
+            "5JU9ytZJyR3zmClGmVm9q4Geqbd.jpg",
+            "69Cz9VNQZy39fUE2g0Ggth6SBTM.jpg",
+            "ktyVmIqfoaJ8w0gDSZyjhhOPpD6.jpg",
+            "t90Y3G8UGQp0f0DrP60wRu9gfrH.jpg",
+            "z3nGs7UED9XlqUkgWeT4jQ80m1N.jpg",
+            "q0R4crx2SehcEEQEkYObktdeFy.jpg",
+            "aAmfIX3TT40zUHGcCKrlOZRKC7u.jpg",
+            "nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg"
     };
 }
