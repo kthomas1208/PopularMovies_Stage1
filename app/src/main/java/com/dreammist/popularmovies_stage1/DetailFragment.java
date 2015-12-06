375b8c34664985b8ef3ca1cd803c492e7058cc85
kevinthomas <kthomas1208@gmail.com>
2015-12-05 22:25:09 -0500

    * Put DetailFragment into its own class.

---
+++ app/src/main/java/com/dreammist/popularmovies_stage1/DetailFragment.java
@@ -0,0 +1,86 @@
+package com.dreammist.popularmovies_stage1;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DetailFragment extends Fragment {
+
+    public DetailFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_detail,container, false);
+
+        final String path = "http://image.tmdb.org/t/p/w185/";
+        Intent intent = getActivity().getIntent();
+
+        if (intent != null && intent.hasExtra("com.dreammist.popularmovies_stage1.Movie")) {
+            Movie movie = (Movie) intent.getParcelableExtra("com.dreammist.popularmovies_stage1.Movie");
+
+            // Title
+            TextView title = (TextView) rootview.findViewById(R.id.movie_title);
+            title.setText(movie.title);
+
+            // Poster
+            String url = path + movie.getPosterPath();
+            View poster = rootview.findViewById(R.id.detail_movie_poster);
+            Picasso.with(container.getContext()).load(url).into((ImageView) poster);
+
+            // Year
+            TextView year = (TextView) rootview.findViewById(R.id.movie_year);
+            String releaseDate = movie.releaseDate;
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = null;
+            String releaseYearStr = "";
+            try {
+                date = sdf.parse(releaseDate);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            if (date != null) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(date);
+                int releaseYear = cal.get(Calendar.YEAR);
+                releaseYearStr = Integer.toString(releaseYear);
+            }
+            if (!releaseYearStr.isEmpty()) year.setText(releaseYearStr);
+
+            // Rating
+            RatingBar rating = (RatingBar) rootview.findViewById(R.id.ratingBar);
+            rating.setStepSize((float)0.01);
+            float ratingScaled = (movie.voteAverage*5)/10;
+            rating.setRating(ratingScaled);
+
+            LayerDrawable stars = (LayerDrawable) rating.getProgressDrawable();
+            stars.getDrawable(0).setColorFilter(Color.parseColor("#c5ae3b"), PorterDuff.Mode.SRC_ATOP);
+            stars.getDrawable(1).setColorFilter(Color.parseColor("#c5ae3b"), PorterDuff.Mode.SRC_ATOP);
+            stars.getDrawable(2).setColorFilter(Color.parseColor("#c5ae3b"), PorterDuff.Mode.SRC_ATOP);
+
+            // Description
+            TextView description = (TextView) rootview.findViewById(R.id.movie_description);
+            String overViewText = movie.overview;
+            if(overViewText.equalsIgnoreCase("null")) overViewText = getString(R.string.no_overview);
+
+            description.setText(overViewText);
+        }
+
+        return rootview;
+    }
+}
